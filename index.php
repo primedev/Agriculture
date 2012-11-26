61558499db1c418b9382cf4a1027de2a78d41c0c
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-26 18:00:47 +0330

    add html for logo

--- index.php
+++ index.php
@@ -62,7 +62,9 @@
 					</ul>
 				</nav>
 				<section class="logo mr20">
-					<a href="./index.php" title="Agriculture" class="logo"></a>
+					<a href="./index.php" title="Agriculture" class="logo">
+						<h1>Agrculture</h1>
+					</a>
 				</section>
 			</section>
 
