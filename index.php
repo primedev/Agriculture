c913831c82f3b0b1ec5377162419ab39c522e91f
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-26 19:16:58 +0330

    add html for section content sideright

--- index.php
+++ index.php
@@ -76,8 +76,19 @@
 				
 			</section>
 		</header>
-		<section class="content">
-			
+		<section class="content cover">
+			<div class="bg-top"></div>
+
+			<section class="sideright right last">	
+				<a href="#" title="search" class="tw"></a>
+				<div class="search-area">
+					<input type="text" class="search" id="search" style="color: rgb(188, 188, 188);" placeholder="جستجو" />
+					<input type="button" name="search" value="جستجو" class="btn-search">
+				</div>
+			</section>
+
+			<section class="sideleft"></section>
+
 		</section>
 		<footer>
 			© 2010  •  
