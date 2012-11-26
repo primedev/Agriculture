ad57b8ec52e65f16a6baad6ec1399cdd280520f8
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-26 12:51:45 +0330

    add files....

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Agricukture</title>
+	<link href="1styles.css" rel="stylesheet" type="text/css" />
+	<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+	<link href="style.less" rel="stylesheet/less" type="text/css" />
+	<link type="text/css"rel="stylesheet" href="style-slide-top.css"/>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	
+	<script type="text/javascript">
+		document.write('<script type="text/javascript" src=scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>');
+	</script>
+	<script type="text/javascript" src="scripts/less.js">
+	</script>
+	<script type="text/javascript" src="scripts/script.js">
+	</script>
+	<!--[if IE]>
+		<script type="text/javascript" src="/scripts/html5.js"></script>
+	<![endif]-->
+	<script type="text/javascript">
+		window.onload = function(){
+			document.getElementById('loading').style.display='none';
+		}
+	</script>
+	<style type="text/css">
+		@font-face {
+		    font-family: 'Nazanin';
+		    src: url('fonts/BNazanin.eot');
+		    src: url('fonts/BNazanin.eot?#iefix') format('embedded-opentype'),
+		         url('fonts/BNazanin.woff') format('woff'),
+		         url('fonts/BNazanin.ttf') format('truetype');
+		    font-weight: normal;
+		    font-style: normal;
+		}
+		body {
+			font-family: 'Nazanin', Arial, sans-serif;
+			-webkit-font-smoothing : antialiased;
+		}
+	</style>
+</head>
+<body class="preload">
+	<div id="loading">
+		<img src="images/loading.gif" alt="">
+	</div>
+	
+</body>
+</html>
