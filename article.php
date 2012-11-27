05fb2dc865880571373a37049711bce206f644ae
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-27 14:43:01 +0330

    complete article

--- article.php
+++ article.php
@@ -3,10 +3,17 @@
 	include "topheader.php";
 ?>
 <section class="content cover">
-			<div class="bg-top"></div>
+	<div class="bg-top"></div>
 <?php
 	include "sidebarright.php";
 ?>
+	<section class="sideleft">
+		<?php 
+			include "post.php";
+			include "post.php";
+			include "post.php";
+		?>
+	</section>
 </section>
 <?php
 	include "footer.php";
