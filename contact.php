c92fd4ffa68431f1d7f207f66490eea5f3280de7
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-27 15:12:51 +0330

    change some html for contact.php

--- contact.php
+++ contact.php
@@ -7,6 +7,18 @@
 <?php
 	include "sidebarright.php";
 ?>
+<section class="sideleft">
+	<div class="post">
+		<div class="post-title">
+			<h2>ارتباط با ما</h2>
+		</div>
+		<p class="f12">ارتباط با ماارتباط با ماارتباط با ماارتباط با ماارتباط با ماارتباط با ماارتباط با ماارتباط با ماارتباط با ماارتباط با ماارتباط با ماارتباط با ماارتباط با ماارتباط با ماارتباط با ماارتباط با ماارتباط با ماارتباط با ماارتباط با ماارتباط با ما</p>
+		<div class="divseprate"></div>
+		<div class="post-title">
+			<h2>ارسال ایمیل</h2>
+		</div>
+	</div>
+</section>
 </section>
 <?php
 	include "footer.php";
