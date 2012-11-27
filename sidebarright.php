296da9a4743baaaab6dd7f1cc45a52a05c800361
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-27 13:45:21 +0330

    create file sidebar right

---
+++ sidebarright.php
@@ -0,0 +1,7 @@
+<section class="sideright right last">	
+	<a href="#" title="search" class="tw"></a>
+	<div class="search-area">
+		<input type="text" class="search" id="search" style="color: rgb(188, 188, 188);" placeholder="جستجو" />
+		<input type="button" name="search" value="جستجو" class="btn-search">
+	</div>
+</section>
