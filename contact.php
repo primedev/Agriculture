907babeb89f1f36bec765e6b30802a64962a88db
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-27 14:35:22 +0330

    change name .....

--- contact.php
+++ contact.php
@@ -7,17 +7,6 @@
 <?php
 	include "sidebarright.php";
 ?>
-	<section class="sideleft">
-		<div class="post">
-			<div class="post-title">
-				<h2 class="f21" style="border-bottom:2px solid #C6C6C6; margin-right:10px;">درباره ما</h2>
-			</div>
-			<p class="f12">درباره درباره درابهر درباره درباره درابهر درباره درباره درابهر درباره درباره درابهر درباره درباره درابهر درباره درباره درابهر دباره درابهر درباره درباره درابهر درباره درباره درابهر درباره درباره درابهر دباره درابهر درباره درباره درابهر درباره درباره درابهر درباره درباره درابهر دباره درابهر درباره درباره درابهر درباره درباره درابهر درباره درباره درابهر درباره درباره درابهر </p>
-			<div class="divseprate">
-				
-			</div>
-		</div>
-	</section>
 </section>
 <?php
 	include "footer.php";
