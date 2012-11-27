cb431e0999ac85156fe2c555e7358fe4d4728558
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-27 13:44:28 +0330

    craete file mainmenu.php

---
+++ mainmenu.php
@@ -0,0 +1,12 @@
+<nav class="main-menu w21 right">
+	<ul class="lavaLamp">
+		<li class="current_page_item"><a href="./index.php">صفحه اصلی</a></li>
+		<li><a href="./about.php">درباره ما</a></li>
+		<li><a href="./article.php">مقالات</a></li>
+		<li><a href="./marketing.php">فروش اسلاید</a></li>
+		<li><a href="./contact.php">ارتباط با ما</a></li>
+		<li class="back" style="right:5px; width:105px; display:block; overflow:hidden;">
+			<div class="imgleft"></div>
+		</li>
+	</ul>
+</nav>
