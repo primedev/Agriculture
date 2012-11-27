027eb516669380a5cddb6d756d15e9f4c2aca926
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-27 13:45:04 +0330

    change some html for seprate code

--- index.php
+++ index.php
@@ -1,91 +1,22 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>Agricukture</title>
-	<link href="1styles.css" rel="stylesheet" type="text/css" />
-	<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
-	<link href="style.less" rel="stylesheet/less" type="text/css" />
-	
-	<script type="text/javascript">
-		document.write('<script type="text/javascript" src=scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>');
-	</script>
-	<script type="text/javascript" src="scripts/less.js">
-	</script>
-	<script type="text/javascript" src="scripts/script.js">
-	</script>
-	<script type="text/javascript" src="scripts/jquery.js">
-	</script>
-	<script type="text/javascript" src="scripts/menuhover.js">
-	</script>
-	<!--[if IE]>
-		<script type="text/javascript" src="/scripts/html5.js"></script>
-	<![endif]-->
-	<script type="text/javascript">
-		window.onload = function(){
-			document.getElementById('loading').style.display='none';
-		}
-	</script>
-	<style type="text/css">
-		@font-face {
-		    font-family: 'Nazanin';
-		    src: url('fonts/BNazanin.eot');
-		    src: url('fonts/BNazanin.eot?#iefix') format('embedded-opentype'),
-		         url('fonts/BNazanin.woff') format('woff'),
-		         url('fonts/BNazanin.ttf') format('truetype');
-		    font-weight: normal;
-		    font-style: normal;
-		}
-		body {
-			font-family: 'Nazanin', Arial, sans-serif;
-			-webkit-font-smoothing : antialiased;
-		}
-	</style>
-</head>
-<body class="preload">
-	<div id="loading">
-		<img src="images/loading.gif" alt="">
-	</div>
-	<section class="container">
-		<header class="cover">
-			<section class="top cover">
-				<nav class="main-menu w21 right">
-					<ul class="lavaLamp">
-						<li class="current_page_item"><a href="#">صفحه اصلی</a></li>
-						<li><a href="#">درباره ما</a></li>
-						<li><a href="#">مقالات</a></li>
-						<li><a href="#">فروش اسلاید</a></li>
-						<li><a href="#">ارتباط با ما</a></li>
-						<li class="back" style="right:5px; width:105px; display:block; overflow:hidden;">
-							<div class="imgleft"></div>
-						</li>
-					</ul>
-				</nav>
-				<section class="logo mr20">
-					<a href="./index.php" title="Agriculture" class="logo">
-						<h1>Agrculture</h1>
-					</a>
-				</section>
-			</section>
-
+<?php 
+	include "header.php";
+?>
+			<?php 
+				include "topheader.php";
+			?>
+		<section class="slide-zoom">
 			<section class="text">
-				
 			</section>
 
 			<section class="box">
-				
 			</section>
-		</header>
+		</section>
 		<section class="content cover">
 			<div class="bg-top"></div>
 
-			<section class="sideright right last">	
-				<a href="#" title="search" class="tw"></a>
-				<div class="search-area">
-					<input type="text" class="search" id="search" style="color: rgb(188, 188, 188);" placeholder="جستجو" />
-					<input type="button" name="search" value="جستجو" class="btn-search">
-				</div>
-			</section>
+			<?php 
+				include "sidebarright.php";
+			?>
 
 			<section class="sideleft">
 				<div class="title">
@@ -97,7 +28,7 @@
 				</div>
 				<div class="post">
 					<div class="post-title">
-					 	<h2><a href="#">مقاله 1 مقاله 1 مقاله</a></h2>
+					 	<h2><a href="#">نام کامل مقاله</a></h2>
 					 	<div class="author">
 					 		نویسنده :
 					 		<span>مدیر</span>
@@ -129,12 +60,6 @@
 			</section>
 
 		</section>
-		<footer>
-			© 2010  •  
-			<a href="#" title="Agriculture">Agriculture</a>
-			  •  All rights reserved
-		</footer>
-	</section>
-	
-</body>
-</html>
+<?php
+	include "footer.php"; 
+?>
