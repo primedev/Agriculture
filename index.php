712d54c27f2e14850dd6d475e275f87939ec7c7e
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-27 12:20:16 +0330

    add css for post complete

--- index.php
+++ index.php
@@ -96,13 +96,35 @@
 					<p>در این وب سایت ..در این وب سایت ..در این وب سایت ..در این وب سایت ..در این وب سایت .. در این وب سایت .. در این وب سایت .. در این وب سایت .. در این وب سایت .. .</p>
 				</div>
 				<div class="post">
-					 <div class="post-title">
+					<div class="post-title">
 					 	<h2><a href="#">مقاله 1 مقاله 1 مقاله</a></h2>
 					 	<div class="author">
 					 		نویسنده :
 					 		<span>مدیر</span>
 					 	</div>
-					 </div>
+					 	<div class="date">
+					 		<span class="number">12</span>
+					 		<span class="month">اردیبهشت</span>
+					 	</div>
+					 	<div class="comments">
+					 		<a href="#" class="count-cmnt">
+					 			<p>7</p>
+					 		</a>
+					 		نظرات
+					 	</div>
+					</div>
+					<div class="post-content">
+						<div class="img-post">
+							<a href="#">
+								<img src="./images/thumb.gif" width="161" height="137" />
+							</a>
+						</div>
+						<p>در این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایت</p>
+						<a href="#" class="more">
+							<b></b>
+							ادامه مطلب 
+						</a>
+					</div>
 				</div>
 			</section>
 
