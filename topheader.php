41da4f02d5f2f405cf06869a6c144a2a56cfc5bd
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-27 13:43:11 +0330

    seprate file & create topheader fro top all pages

---
+++ topheader.php
@@ -0,0 +1,14 @@
+<header class="cover">
+	<section class="top cover">
+			
+		<?php 
+			include "mainmenu.php";
+		?>
+		<section class="logo mr20">
+			<a href="./index.php" title="Agriculture" class="logo">
+				<h1>Agrculture</h1>
+			</a>
+		</section>
+
+	</section>
+</header>
