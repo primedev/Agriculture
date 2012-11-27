ce4f797af9deab41508dce4bf1b1e140be837d5e
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-27 14:56:51 +0330

    add html for page specialarticle

--- specialarticle.php
+++ specialarticle.php
@@ -7,6 +7,20 @@
 <?php
 	include "sidebarright.php";
 ?>
+	<section class="sideleft">
+		<div class="post">
+			<div class="post-title">
+				<h2>نام کامل مقاله</h2>
+			</div>
+			<p>
+				<img src="./images/pic_big.jpg" class="pic" />
+			</p>
+			<p class="f12"> متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله </p>
+			<div class="divseprate">
+				
+			</div>
+		</div>
+	</section>
 </section>
 <?php
 	include "footer.php";
