5c397f53630d3768e235bfa87e74224c216f222f
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-29 12:07:46 +0330

    add some html for marketing...

--- marketing.php
+++ marketing.php
@@ -11,7 +11,10 @@
 			<ul id="marketing-category">
 				<li>
 					<div class="img-marketing">
-						<a href="./images/leaf.jpg" class="gall"></a>
+						<a href="./images/leaf.jpg" class="gall">
+							<img src="images/portfolio2_thumb1.jpg" width="371" height="182" class="a" style="display:block" />
+							<img src="images/portfolio_thumb_hover2.png" width="371" height="182" class="b" style="opacity : 0" />
+						</a>
 					</div>
 				</li>
 			</ul>	
