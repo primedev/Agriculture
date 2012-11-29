d3064199ea22854439fbf901d2d45b2e54a0cbc4
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-29 12:58:27 +0330

    optimal js

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,13 @@
 $(function(){
 	var $MarketingImg = $('div.img-marketing a.gall img.b');
 
-	$MarketingImg.mouseover(function(){
+	$MarketingImg.hover(function(){
 		$(this).animate({
 			opacity : 0.5
 		},700);
-	});
-
-	$MarketingImg.mouseout(function(){
+	},function(){
 		$(this).animate({
-			opacity : 0
-		},500);
+	 		opacity : 0
+	 	},500);
 	});
 });
