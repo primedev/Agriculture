6d869bfc28cb58f769b602f924b2dcd4e32a85f0
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-29 17:34:40 +0330

    change some html for main menu

--- mainmenu.php
+++ mainmenu.php
@@ -5,8 +5,8 @@
 		<li><a href="./article.php">مقالات</a></li>
 		<li><a href="./marketing.php">فروش اسلاید</a></li>
 		<li><a href="./contact.php">ارتباط با ما</a></li>
-		<li class="back" style="right:5px; width:105px; display:block; overflow:hidden;">
-			<div class="imgleft"></div>
-		</li>
 	</ul>
+	<script type="text/javascript">
+		$(function() { $(".lavaLamp").lavaLamp({ fx: "backout", speed: 700}) });
+	</script>
 </nav>
