55f3aab9645defce8976acab0a578f8ec8c623b3
zohreh-z <zahedi.zohreh@gmail.com>
2012-11-29 11:40:47 +0330

    add some html for marketing...

--- marketing.php
+++ marketing.php
@@ -3,8 +3,18 @@
 	include "topheader.php";
 ?>
 	<section class="content-marketing">
-		<div class="marketing">
-			<div class="top-img"></div>	
+		<div class="marketing cover">
+			<div class="top-img"></div>
+			<div class="title">
+				<h1 class="f20">اسلاید ها</h1>	
+			</div>
+			<ul id="marketing-category">
+				<li>
+					<div class="img-marketing">
+						<a href="./images/leaf.jpg" class="gall"></a>
+					</div>
+				</li>
+			</ul>	
 		</div>
 	</section>
 <?php
