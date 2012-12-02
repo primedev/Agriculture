1b71cbb247c77ea977132c82bd1317506c4a6782
zohreh-z <zahedi.zohreh@gmail.com>
2012-12-02 15:05:54 +0330

    without ajax

--- specialarticle.php
+++ specialarticle.php
@@ -1,6 +1,6 @@
 <?php 
 	include "header.php";
-	include "topheader.php";
+	// include "topheader.php";
 ?>
 <section class="content cover">
 	<div class="bg-top"></div>
@@ -8,7 +8,7 @@
 	include "sidebarright.php";
 ?>
 	<section class="sideleft">
-		<div class="ajaxcontent">
+		<!-- <div class="ajaxcontent"> -->
 		<div class="post">
 			<div class="post-title">
 				<h2>نام کامل مقاله</h2>
@@ -19,7 +19,7 @@
 			<p class="f12"> متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله  متن کامل مقاله </p>
 			<div class="divseprate"></div>
 		</div>
-		</div>
+		<!-- </div> -->
 	</section>
 </section>
 <?php
