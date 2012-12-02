a9d499c771321c205c2a271b246a95a9daca1017
zohreh-z <zahedi.zohreh@gmail.com>
2012-12-02 14:38:11 +0330

    delete tabs....

--- post.php
+++ post.php
@@ -1,31 +1,31 @@
 <div class="post">
-					<div class="post-title">
-					 	<h2 style="margin-right:50px; border:none;"><a href="#">نام کامل مقاله</a></h2>
-					 	<div class="author">
-					 		نویسنده :
-					 		<span>مدیر</span>
-					 	</div>
-					 	<div class="date">
-					 		<span class="number">12</span>
-					 		<span class="month">اردیبهشت</span>
-					 	</div>
-					 	<div class="comments">
-					 		<a href="#" class="count-cmnt">
-					 			<p>7</p>
-					 		</a>
-					 		نظرات
-					 	</div>
-					</div>
-					<div class="post-content">
-						<div class="img-post">
-							<a href="#">
-								<img src="./images/thumb.gif" width="161" height="137" />
-							</a>
-						</div>
-						<p>در این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایت</p>
-						<a href="./specialarticle.php" class="more">
-							<b></b>
-							ادامه مطلب 
-						</a>
-					</div>
-				</div>
+	<div class="post-title">
+	 	<h2 style="margin-right:50px; border:none;"><a href="#">نام کامل مقاله</a></h2>
+	 	<div class="author">
+	 		نویسنده :
+	 		<span>مدیر</span>
+	 	</div>
+	 	<div class="date">
+	 		<span class="number">12</span>
+	 		<span class="month">اردیبهشت</span>
+	 	</div>
+	 	<div class="comments">
+	 		<a href="#" class="count-cmnt">
+	 			<p>7</p>
+	 		</a>
+	 		نظرات
+	 	</div>
+	</div>
+	<div class="post-content">
+		<div class="img-post">
+			<a href="#">
+				<img src="./images/thumb.gif" width="161" height="137" />
+			</a>
+		</div>
+		<p>در این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایتدر این سایت</p>
+		<a href="./specialarticle.php" class="more">
+			<b></b>
+			ادامه مطلب 
+		</a>
+	</div>
+</div>
