f56395e9e519ac6f23317e1c6a5d4aff21695d27
zohreh-z <zahedi.zohreh@gmail.com>
2012-12-22 13:10:29 +0330

    lick ok

--- post.php
+++ post.php
@@ -1,6 +1,6 @@
 <div class="post">
 	<div class="post-title">
-	 	<h2 style="margin-right:50px; border:none;"><a href="#">نام کامل مقاله</a></h2>
+	 	<h2 style="margin-right:50px; border:none;"><a href="./specialarticle.php" target="_blank">نام کامل مقاله</a></h2>
 	 	<div class="author">
 	 		نویسنده :
 	 		<span>مدیر</span>
